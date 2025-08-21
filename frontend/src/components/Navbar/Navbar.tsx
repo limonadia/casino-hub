180a08fb98990ca5591d0f2a393d9721b9d83b49
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-08-21 10:49:11 +0200

    Import tailwind.

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
-
-// ---- Navbar ----
 function Navbar({ balance }: { balance: number }) {
 return (
-<nav className="flex justify-between items-center p-4 bg-gray-900 text-white">
-<h1 className="text-2xl font-bold">Casino Hub</h1>
-<div className="flex gap-6">
-<Link to="/">Home</Link>
-<Link to="/games">Games</Link>
-<Link to="/profile">Profile</Link>
+<div className="flex flex-row justify-between items-center p-4 bg-gray-900 ">
+    <div className="">
+        <h1 className="text-2xl font-bold w-max text-red">Casino Hub</h1>
+    </div>
+    <div className="flex gap-6">
+        <Link to="/">Home</Link>
+        <Link to="/games">Games</Link>
+        <Link to="/profile">Profile</Link>
+        <div className="text-red-500  text-red">💰 Balance: {balance}</div>
+    </div>
 </div>
-<div>💰 Balance: {balance}</div>
-</nav>
 );
 }
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-    <div className="p-6 text-center">
+    <div className="p-6 text-center page">
     <h2 className="text-3xl font-bold mb-4">Welcome to Casino Hub 🎰</h2>
     <p>Select a game and try your luck with fake coins!</p>
     </div>
