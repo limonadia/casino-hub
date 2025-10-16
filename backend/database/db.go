8f023a035bc5618f31c1e77b02d7e8b70d9128e2
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-10-16 14:05:31 +0200

    -

--- backend/database/db.go
+++ backend/database/db.go
@@ -2,11 +2,10 @@ package database
 
 import (
     "database/sql"
-    "fmt"
     "log"
-    "net/url"
-    _ "github.com/lib/pq"
     "os"
+    _ "github.com/lib/pq"
+    "strings"
 )
 
 var DB *sql.DB
@@ -17,29 +16,16 @@ func InitDB() {
         log.Fatal("DATABASE_URL environment variable not set")
     }
 
-    // Parse DATABASE_URL to extract connection info
-    u, err := url.Parse(dbURL)
-    if err != nil {
-        log.Fatalf("Invalid DATABASE_URL: %v", err)
+    if !strings.Contains(dbURL, "sslmode=") {
+        dbURL += "?sslmode=require"
     }
 
-    user := u.User.Username()
-    password, _ := u.User.Password()
-    host := u.Hostname()
-    port := u.Port()
-    dbname := u.Path[1:] // remove leading "/"
-
-    connStr := fmt.Sprintf(
-        "host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-        host, port, user, password, dbname,
-    )
-
-    DB, err = sql.Open("postgres", connStr)
+    var err error
+    DB, err = sql.Open("postgres", dbURL)
     if err != nil {
         log.Fatalf("Failed to connect to database: %v", err)
     }
 
-    // Test connection
     if err := DB.Ping(); err != nil {
         log.Fatalf("Failed to ping database: %v", err)
     }
